2c8da8db49c69d8390b7ac2fccf9fc52f9a268a4
Suna Choi <su00021zu@gmail.com>
2024-09-26 12:47:48 +0900

    etDetailImage

--- src/app/ETDetail/page.jsx
+++ src/app/ETDetail/page.jsx
@@ -1,17 +1,19 @@
+import Image from 'next/image';
+
 import styles from './ETDetail.module.css';
 
 const ETDetail = () => {
   return (
     <div className={styles.hero}>
       <div className={styles.heroContent}>
-        <section className={styles.box}>
+        {/* <section className={styles.box}>
           <div className={styles.content}>
             <h2 className={styles.heading}>EcoTrade</h2>
             <p className={styles.p}>
               EcoTrade(에코 트레이드)는 여러분을 위한 녹색 중고거래 플랫폼입니다.
             </p>
           </div>
-        </section>
+        </section> */}
 
         <section className={styles.box}>
           <div className={styles.content}>
@@ -20,18 +22,34 @@ const ETDetail = () => {
               자리차지만 하고 있는 잡동사니, 쓰레기로 버리기 보다는 필요한 사람을 찾아보세요!
             </p>
           </div>
-          <div className={styles.imagePlaceholder}>이미지 삽입</div>
+          <div className={styles.imagePlaceholder}>
+          <Image src="/images/중고거래이미지.png"
+            alt="중고거래이미지"
+            width={200}
+            height={200} />
+          </div>
         </section>
 
         <section className={styles.box}>
           <div className={styles.content}>
             <h2 className={styles.heading}>어떻게 거래하나요?</h2>
             <p className={styles.p}>
-              에코트레이드는 채팅과 지도를 이용한 직거래 시스템을 지원합니다. 당신 근처에서
+              에코트레이드는 채팅을 이용한 직거래 시스템을 지원합니다. 당신 근처에서
               시작해보세요!
             </p>
           </div>
-          <div className={styles.imagePlaceholder}>이미지 삽입</div>
+          
+          <div className={styles.imagePlaceholder}>
+          <Image src="/images/채팅아이콘.png"
+            alt="채팅아이콘"
+            width={100}
+            height={100} />
+          <Image src="/images/직거래아이콘.png"
+            alt="직거래아이콘"
+            width={100}
+            height={100} />
+
+          </div>
         </section>
 
         <section className={styles.box}>
@@ -49,7 +67,20 @@ const ETDetail = () => {
             </p>
             <div className={styles.sns}>sns link삽입</div>
           </div>
-          <div className={styles.imagePlaceholder}>이미지 삽입</div>
+          <div className={styles.imagePlaceholder}>
+            <Image src="/images/EcoPoint친환경상품구매아이콘.png"
+            alt="친환경상품구매아이콘"
+            width={100}
+            height={100} />
+            <Image src="/images/EcoPoint나무심기아이콘.png"
+            alt="나무심기아이콘"
+            width={100}
+            height={100} />
+            <Image src="/images/EcoPoint탄소절감아이콘.png"
+            alt="탄소절감아이콘"
+            width={100}
+            height={100} />
+          </div>
         </section>
       </div>
     </div>
